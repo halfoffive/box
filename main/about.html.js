ebc34e40164cab07461d0bdcc09d057f662815fa
niaodtiantang <niaodtiantang@foxmail.com>
2022-07-03 11:02:33 +0800

    提升

--- main/about.html.js
+++ main/about.html.js
@@ -1,5 +1,14 @@
 //适用于about.html
-let index_zh_cn="";//显示的内容
+let index_zh_cn="<h1>关于</h1>" +
+    "<h2>简介</h2>" +
+    "<p>本工具箱是由<a href='https://www.github.com/niaodtiantang'>niaodtiantang</a>开发的在线工具箱，完全，免费无广告。目前拥有2个工具。</p>" +
+    "<h2>开源</h2>" +
+    "<h3>主题</h3>" +
+    "<p>本站主题基于<a href='https://mdui.org'>mdui</a>。</p>" +
+    "<h3>协议</h3>" +
+    "<p>遵循<a href='http://www.apache.org/licenses/'>Apache License 2.0</a>。</p>" +
+    "<h3>源码</h3>" +
+    "<p>你可以在github上查看源码。<a href='https://www.github.com/niaodtiantang/box/'><img src="https://ndtt.vercel.app/api/pin/?username=niaodtiantang&repo=box&show_owner=true"></a> </p>";//显示的内容
 document.getElementById("main").innerHTML=index_zh_cn;//替换内容
 
 //所需函数和脚本
